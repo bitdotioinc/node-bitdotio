4ae2a25373d0942606cfd0a940e0bf423ec3eefe
George Danforth <george@bit.io>
2022-11-30 12:29:06 -0500

    Reformat

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,7 @@
-export function splitDbName(fullName: string): { username: string; db_name: string } {
+export function splitDbName(fullName: string): {
+  username: string;
+  db_name: string;
+} {
   const [username, ...rest] = fullName.split("/");
   return { username, db_name: encodeURIComponent(rest.join("/")) };
 }
