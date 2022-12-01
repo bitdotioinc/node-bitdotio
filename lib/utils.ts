9a7e19d225251739cfd5913927322e39b0051f4f
George Danforth <george@bit.io>
2022-11-30 19:46:16 -0500

    Add db name and token validation with tests

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,19 @@
+const RE_TOKEN = /^v2_\w+$/;
+const RE_DB_NAME = /^\w.{0,37}?\/.{1,23}$/;
+
 export function splitDbName(fullName: string): {
   username: string;
   db_name: string;
 } {
+  if (!RE_DB_NAME.test(fullName)) {
+    throw new Error("Invalid database name");
+  }
   const [username, ...rest] = fullName.split("/");
   return { username, db_name: encodeURIComponent(rest.join("/")) };
+}
+
+export function validateToken(token: string) {
+  if (!RE_TOKEN.test(token)) {
+    throw new Error("Invalid token");
+  }
 }
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,4 @@
-import { splitDbName } from "../lib/utils";
+import { splitDbName, validateToken } from "../lib/utils";
 
 describe("Utils module", () => {
   test("splitDbName base case", () => {
@@ -11,9 +11,34 @@ describe("Utils module", () => {
     expect(username).toBe("a");
     expect(db_name).toBe("b%2Fc");
   });
-  test("splitDbName no slash", () => {
-    const { username, db_name } = splitDbName("foo");
-    expect(username).toBe("foo");
-    expect(db_name).toBe("");
+  test("splitDbName invalid db name", () => {
+    const testCases = [
+      "/mydb",
+      "mydb",
+      "myExtremelyLongUserNameWhichIsDefinitelyWayTooLong/db",
+      "me/myExtremelyLongDatabaseNameWhichIsDefinitelyWayTooLong",
+      "-/db",
+      "./db",
+      "me/",
+    ];
+    testCases.forEach((testCase) => {
+      expect(() => splitDbName(testCase)).toThrow("Invalid database name");
+    });
+  });
+
+  test("validateToken ok", () => {
+    expect(validateToken("v2_testtoken")).toBeUndefined();
+  });
+  test("validateToken invalid token", () => {
+    const testCases = [
+      "v2_",
+      "v1_testtoken",
+      "v2__.--_--",
+      "v2testtoken",
+      "testtoken",
+    ];
+    testCases.forEach((testCase) => {
+      expect(() => validateToken(testCase)).toThrow("Invalid token");
+    });
   });
 });
